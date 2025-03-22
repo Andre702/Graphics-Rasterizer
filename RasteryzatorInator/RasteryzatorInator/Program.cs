fa798c9ae4ba1d6c6802afbc8b5428ef5e09d4b8
Filip A. <filip.andrzejewski@o2.pl>
2025-03-22 15:02:14 +0100

    Task 1 Done

--- RasteryzatorInator/RasteryzatorInator/Program.cs
+++ RasteryzatorInator/RasteryzatorInator/Program.cs
@@ -1,16 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-namespace RasteryzatorInator
+namespace RasteryzatorInator;
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
-        {
-            Console.WriteLine("Hello, World!");
-            Buffer buffer = new Buffer();
-            buffer.SetSize(256, 256);
-            buffer.ClearColor(0, 255, 200);
-            buffer.ClearDepth(1.0f);
-            buffer.SaveTGA("output.tga");
-        }
+        Console.WriteLine("Hello There!");
+        Buffer buffer = new Buffer();
+        buffer.SetSize(256, 512);
+        buffer.ClearColor(0, 255, 200);
+        buffer.ClearDepth(1.0f);
+        buffer.SaveTGA("output.tga");
     }
 }
